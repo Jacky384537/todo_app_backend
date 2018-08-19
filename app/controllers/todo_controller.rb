7d98802221f45c826f574f43003c5e8118bee2a0
Jacky Liang <jackyliang013@gmail.com>
2018-08-19 17:27:08 +0000

    table

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,27 +3,6 @@ class TodoController < ApplicationController
     end
     
     def show
-        if params[:id]=="1"
-            @todo_description="Make the curricum"
-            @todo_pomodoro_estimate=4
-        elsif params[:id]=="2"
-            @todo_description="Buy workshop supplies"
-            @todo_pomodoro_estimate=3
-        elsif params[:id]=="3"
-            @todo_description="Meet with the volunteer trainers"
-            @todo_pomodoro_estimate=2
-        elsif params[:id]=="4"
-            @todo_description="Order food for Saturday and Sunday"
-            @todo_pomodoro_estimate=1
-        elsif params[:id]=="5"
-            @todo_description="Check pre-work assignments"
-            @todo_pomodoro_estimate=2
-        elsif params[:id]=="6"
-            @todo_description="Send workshop location to all the students"
-            @todo_pomodoro_estimate=1
-        elsif params[:id]=="7"
-            @todo_description="Have a great workshop "
-            @todo_pomodoro_estimate=0
-        end
+        @t=Todo.find(params[:id])
     end
 end
